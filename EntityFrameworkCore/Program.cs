727ec2a8a04d9e0c4832230e039bc50d7b2dc616
Giuliano Masi <g.masic@novomatic.it>
2017-06-28 11:46:37 +0200

    database write and query

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -10,6 +10,26 @@ namespace EntityFrameworkCore
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                var x = db.Database;
+
+                db.Blogs.Add(new Blog() {Url = "https://www.benday.com/2017/02/14/walkthrough-entity-framework-core-1-1-with-migrations/" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine("");
+                Console.WriteLine("All blog inside DB");
+                foreach (var r in db.Blogs)
+                    Console .WriteLine("- {0}", r.Url);
+
+            }
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
